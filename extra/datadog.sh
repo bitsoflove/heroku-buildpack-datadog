461bc29fd7190f5aed0efc7757b60561bdb8d228
Emeric Warnez <emericwarnez@bitsoflove.be>
2024-02-16 11:18:22 +0100

    feat: add logs

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -120,6 +120,8 @@ if [ -z "$DD_HOSTNAME" ]; then
     fi
 
     D="$(echo "$DYNO" | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g')"
+    echo "test"
+    echo "$D"
     export DD_HOSTNAME="$HAN.$D"
   else
     # Set the hostname to the dyno host
